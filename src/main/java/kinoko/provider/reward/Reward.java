32f2849af803836ce14eaf7f26502d4fe4a85e9c
iw2d <30405132+iw2d@users.noreply.github.com>
2025-06-08 14:09:28 +1000

    Implement rewards with field requirement (#49), added drops for the "Who Broke the Seal of xxxxx" quests (Ripped Paper 1-5)
    - Added drops for Blue Ribbon Pig
    - Note: the quest help text for Who Broke the Seal of Crocell (28260) is broken and points to 100030000, which was removed in the Big Bang update. The quest should point to Beach : Blue Ribbon Beach (120020300)

--- src/main/java/kinoko/provider/reward/Reward.java
+++ src/main/java/kinoko/provider/reward/Reward.java
@@ -6,13 +6,15 @@ public final class Reward {
     private final int max;
     private final double prob;
     private final int questId;
+    private final int fieldId;
 
-    public Reward(int itemId, int min, int max, double prob, int questId) {
+    public Reward(int itemId, int min, int max, double prob, int questId, int fieldId) {
         this.itemId = itemId;
         this.min = min;
         this.max = max;
         this.prob = prob;
         this.questId = questId;
+        this.fieldId = fieldId;
     }
 
     public int getItemId() {
@@ -35,12 +37,20 @@ public final class Reward {
         return questId;
     }
 
+    public int getFieldId() {
+        return fieldId;
+    }
+
     public boolean isMoney() {
         return itemId == 0;
     }
 
     public boolean isQuest() {
         return questId != 0;
+    }
+
+    public boolean isFieldRequirement() {
+        return fieldId != 0;
     }
 
     @Override
@@ -51,18 +61,23 @@ public final class Reward {
                 ", max=" + max +
                 ", prob=" + prob +
                 ", questId=" + questId +
+                ", fieldId=" + fieldId +
                 '}';
     }
 
     public static Reward item(int itemId, int min, int max, double prob) {
-        return new Reward(itemId, min, max, prob, 0);
+        return new Reward(itemId, min, max, prob, 0, 0);
     }
 
     public static Reward item(int itemId, int min, int max, double prob, int questId) {
-        return new Reward(itemId, min, max, prob, questId);
+        return new Reward(itemId, min, max, prob, questId, 0);
+    }
+
+    public static Reward item(int itemId, int min, int max, double prob, int questId, int fieldId) {
+        return new Reward(itemId, min, max, prob, questId, fieldId);
     }
 
     public static Reward money(int min, int max, double prob) {
-        return new Reward(0, min, max, prob, 0);
+        return new Reward(0, min, max, prob, 0, 0);
     }
 }
