f05c6c0abdd461181ecfe471bedd3da51586e8fd
MujyKun <mujy@irenebot.com>
2025-10-09 21:22:11 -0400

    Added invalid item reference cleanup on bootup

--- src/main/java/kinoko/database/postgresql/PostgresIdAccessor.java
+++ src/main/java/kinoko/database/postgresql/PostgresIdAccessor.java
@@ -2,13 +2,22 @@ package kinoko.database.postgresql;
 
 import com.zaxxer.hikari.HikariDataSource;
 import kinoko.database.IdAccessor;
+import kinoko.database.postgresql.type.ItemDao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public final class PostgresIdAccessor extends PostgresAccessor implements IdAccessor {
 
     public PostgresIdAccessor(HikariDataSource dataSource) {
         super(dataSource);
+
+        try (Connection conn = getConnection()){
+            ItemDao.cleanupInvalidItems(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private Optional<Integer> getNextId(String type) {
