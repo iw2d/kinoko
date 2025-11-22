2f960b2e562caced3e28c53628654ceb53aa7422
iw2d <30405132+iw2d@users.noreply.github.com>
2025-11-23 07:43:37 +1000

    Read PortalInfo->reactorName

--- src/main/java/kinoko/server/command/Arguments.java
+++ src/main/java/kinoko/server/command/Arguments.java
@@ -1,6 +1,10 @@
 package kinoko.server.command;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Annotation for command arguments.
